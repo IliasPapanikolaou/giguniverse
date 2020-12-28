7f03d4d4644ac051ee86c021f3955c78cd85abf6
Ilias <eliaspapanikolaou@hotmail.com>
2020-12-28 18:30:56 +0200

    Swagger and WebConfig added

--- src/main/java/com/unipi/giguniverse/GiguniverseApplication.java
+++ src/main/java/com/unipi/giguniverse/GiguniverseApplication.java
@@ -1,11 +1,14 @@
 package com.unipi.giguniverse;
 
+import com.unipi.giguniverse.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class GiguniverseApplication {
 
 	public static void main(String[] args) {
