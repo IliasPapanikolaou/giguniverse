57a9a308982c62fd38b00d356eb534959dbec508
Ilias <eliaspapanikolaou@hotmail.com>
2021-04-03 00:00:34 +0300

    minor resourcePath bug

--- src/main/db/query/99.Create_temp.sql
+++
@@ -1,9 +0,0 @@
--- Create table temp to load images
-
-create table temp
-(
-    id         int auto_increment
-        primary key,
-    image      longblob null,
-    concert_id int      null
-);
--- src/main/db/query/99.Update_concerts_from_temp.sql
+++
@@ -1,9 +0,0 @@
--- Create base64 from image loaded in temp table
-
-update concert
-inner join temp on temp.concert_id=concert.concert_id
-set concert.image= to_base64(temp.image)
-where temp.concert_id=concert.concert_id
-
-
---COMMIT;
